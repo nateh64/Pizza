f68d15c547ad4b3bd3f2be0943b350d2444d2953
nateh64 <7nhuzenis@stpatrick.org>
2016-11-17 13:57:14 -0600

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,17 @@
+class pizza{
+  //type constructor below
+constructor (size, meatToppings,veggieToppings){
+  this.size=size;
+  this.meatToppings=meatToppings;
+  this.veggieToppings;
+}
+
+
+  //type instance functions below
+
+
+
+  //type class functions below
+
+
+}
