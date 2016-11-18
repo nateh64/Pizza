dcba89bf064c5f288bbb8484e5cb58a151eb5cfd
nateh64 <7nhuzenis@stpatrick.org>
2016-11-18 13:52:22 -0600

    Finished instance and class functions

--- pizza.js
+++ pizza.js
@@ -8,10 +8,35 @@ constructor (size, meatToppings,veggieToppings){
 
 
   //type instance functions below
-
-
+sizeCost(){
+  else if (this.size =="s"){
+  this.sizeCost=7.99;
+  return this.sizeCost;
+}
+else if (this.size=="m") {
+  this.sizeCost=9.99;
+  return this.sizeCost;
+}
+else if (this.size=="l"){
+  this.sizeCost=12.99;
+  return this.sizeCost;
+}
+else if (this.sizeCost=="xl"){
+  this.sizeCost=15.99;
+  return this.sizeCost;
+}
 
   //type class functions below
+toppingCost(top){
+  if(top == "veggie"){
+    return this.veggieToppings.length*.50;
+  }
+  else if(top == "meat"){
+    return this.meatToppings.length*.99;
+  }
+}
 
 
 }
+
+let mypizza = new Pizza("L",["peperoni"],["onion"]);
